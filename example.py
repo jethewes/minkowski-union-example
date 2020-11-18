2d51403afe2290f6453a470b0d503b764cc351e9
jethewes <39996356+jethewes@users.noreply.github.com>
2020-11-18 13:55:02 -0600

    Update example.py
    make selecting a device more easily configurable

--- example.py
+++ example.py
@@ -1,19 +1,21 @@
 import torch, MinkowskiEngine as ME
 from model import MobileNet
 
-torch.cuda.set_device("cuda:2")
+device = "cuda:0" # or whichever GPU you want!
+
+torch.cuda.set_device(device)
 data = torch.load('data/example.pt')
 
-model = MobileNet(2, torch.nn.ReLU(), 1, 1, 4).to("cuda:2")
+model = MobileNet(2, torch.nn.ReLU(), 1, 1, 4).to(device)
 model.train()
 
-test = ( data['xfeats'].to("cuda:2"),
+test = ( data['xfeats'].to(device),
          ME.utils.batched_coordinates([data['xcoords']]),
-         data['yfeats'].to("cuda:2"),
+         data['yfeats'].to(device),
          ME.utils.batched_coordinates([data['ycoords']]))
 
 test = model(test)
-loss = torch.nn.modules.loss.CrossEntropyLoss()(test, data['truth'].reshape([1]).to(test.device))
+loss = torch.nn.modules.loss.CrossEntropyLoss()(test, data['truth'].reshape([1]).to(device))
 print(loss)
 loss.backward()
 
