f41f48b7b211e6705cd358d9aad5dc8a36b1cf4a
chrischoy <cchoy@nvidia.com>
2020-12-27 10:57:47 +0900

    fix minor errors

--- model.py
+++ model.py
@@ -2,13 +2,19 @@ import torch
 from torch.nn import Sequential as Seq, Dropout, Linear, ReLU, Softmax
 import MinkowskiEngine as ME
 
+
 class MinkowskiActivation(ME.MinkowskiNetwork):
     def __init__(self, n_dims, a, **params):
         super(MinkowskiActivation, self).__init__(n_dims)
         self.a = a
 
     def forward(self, x):
-        return ME.SparseTensor(self.a(x.F), coords_key=x.coords_key, coords_manager=x.coords_man)
+        return ME.SparseTensor(
+            self.a(x.F),
+            coordinate_map_key=x.coordinate_map_key,
+            coordinate_manager=x.coordinate_manager,
+        )
+
 
 class Conv(ME.MinkowskiNetwork):
     def __init__(self, D, A, in_feat, out_feat, kernel_size=1, stride=1):
@@ -20,13 +26,16 @@ class Conv(ME.MinkowskiNetwork):
                 out_channels=out_feat,
                 kernel_size=kernel_size,
                 stride=stride,
-                has_bias=False,
-                dimension=D),
+                bias=False,
+                dimension=D,
+            ),
             ME.MinkowskiBatchNorm(out_feat),
-            MinkowskiActivation(D, A))
-        
+            MinkowskiActivation(D, A),
+        )
+
     def forward(self, x):
         return self.net(x)
+
 
 class Bottleneck(ME.MinkowskiNetwork):
     def __init__(self, D, A, in_feat, out_feat, alpha, t, kernel_size, stride, r):
@@ -37,7 +46,7 @@ class Bottleneck(ME.MinkowskiNetwork):
         self.r = r
         self.net = Seq(
             Conv(D, A, in_feat, expansion, 1, 1),
-            #ME.MinkowskiChannelwiseConvolution(
+            # ME.MinkowskiChannelwiseConvolution(
             #    in_channels=expansion,
             #    kernel_size=kernel_size,
             #    stride=stride,
@@ -49,36 +58,47 @@ class Bottleneck(ME.MinkowskiNetwork):
                 out_channels=out_feat,
                 kernel_size=1,
                 stride=1,
-                dimension=D),
-            ME.MinkowskiBatchNorm(out_feat))
-        
+                dimension=D,
+            ),
+            ME.MinkowskiBatchNorm(out_feat),
+        )
+
     def forward(self, x):
         x_out = self.net(x)
-        if self.r: x_out = x_out + x
+        if self.r:
+            x_out = x_out + x
         return x_out
+
 
 class InvertedResidual(ME.MinkowskiNetwork):
     def __init__(self, D, A, in_feat, out_feat, alpha, t, kernel_size, stride, n):
         super(InvertedResidual, self).__init__(D)
-        net = [ Bottleneck(D, A, in_feat, out_feat, alpha, t, kernel_size, stride, False) ]
+        net = [
+            Bottleneck(D, A, in_feat, out_feat, alpha, t, kernel_size, stride, False)
+        ]
         for i in range(1, n):
-            net.append(Bottleneck(D, A, out_feat, out_feat, alpha, t, kernel_size, 1, True))
+            net.append(
+                Bottleneck(D, A, out_feat, out_feat, alpha, t, kernel_size, 1, True)
+            )
         self.net = Seq(*net)
-        
+
     def forward(self, x):
         return self.net(x)
+
 
 class SubNet(ME.MinkowskiNetwork):
     def __init__(self, D, A, alpha):
         super(SubNet, self).__init__(D)
         self.net = Seq(
-            Conv(D, A, 1, int(alpha*32), 3, 2),
+            Conv(D, A, 1, int(alpha * 32), 3, 2),
             InvertedResidual(D, A, 32, 16, alpha, 1, 3, 1, 1),
             InvertedResidual(D, A, 16, 24, alpha, 6, 3, 2, 2),
-            InvertedResidual(D, A, 24, 32, alpha, 6, 3, 2, 3))
-        
+            InvertedResidual(D, A, 24, 32, alpha, 6, 3, 2, 3),
+        )
+
     def forward(self, x):
         return self.net(x)
+
 
 class MobileNet(ME.MinkowskiNetwork):
     def __init__(self, D, A, alpha, depth, classes, **kwargs):
@@ -86,29 +106,32 @@ class MobileNet(ME.MinkowskiNetwork):
 
         self.input_x = SubNet(D, A, alpha)
         self.input_y = SubNet(D, A, alpha)
-        
+
         self.net = Seq(
             InvertedResidual(D, A, 32, 64, alpha, 6, 3, 2, 4),
             InvertedResidual(D, A, 64, 96, alpha, 6, 3, 1, 3),
             InvertedResidual(D, A, 96, 160, alpha, 6, 3, 2, 3),
             InvertedResidual(D, A, 160, 320, alpha, 6, 3, 1, 1),
-            Conv(D, A, int(alpha*320), int(alpha*1280)),
-            ME.MinkowskiGlobalPooling())
-       
+            Conv(D, A, int(alpha * 320), int(alpha * 1280)),
+            ME.MinkowskiGlobalPooling(),
+        )
+
         self.union = ME.MinkowskiUnion()
 
         self.final = Seq(
             Dropout(0.4),
-            Linear(int(alpha*1280), 1024),
+            Linear(int(alpha * 1280), 1024),
             A,
             Dropout(0.4),
-            Linear(1024, classes, bias=False))
-        
-    def forward(self, x):
-        
-        xview = ME.SparseTensor(x[0], x[1])
-        yview = ME.SparseTensor(x[2], x[3])
-        
+            Linear(1024, classes, bias=False),
+        )
+
+    def forward(self, x, device="cuda"):
+        xview = ME.SparseTensor(x[0], x[1], device=device)
+        yview = ME.SparseTensor(
+            x[2], x[3], device=device, coordinate_manager=xview.coordinate_manager
+        )
+
         xview = self.input_x(xview)
         yview = self.input_y(yview)
 
@@ -116,4 +139,3 @@ class MobileNet(ME.MinkowskiNetwork):
 
         x = self.net(x)
         return self.final(x.F)
-        
